ab73f8f96ebc316efe4517cee53a071f93704db7
EC2 Default User <ec2-user@ip-172-31-24-247.ap-northeast-1.compute.internal>
2022-11-29 13:24:52 +0000

    [Add]after_sign_in,up_path_for

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
-  after_sign_in_path_for
+  def after_sign_in_path_for(resource)
+    admin_posts(resource)
+  end
+  
+  def after_sign_up_path_for(resource)
+    root_path(resource)
+  end
+  
+  def after_sign_in_path_for(resource)
+    root_path(resource)
+    
   
 end
