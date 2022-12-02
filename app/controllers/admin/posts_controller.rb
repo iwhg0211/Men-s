c0576a36a6568fcce524e98fd7de85751bedbfeb
EC2 Default User <ec2-user@ip-172-31-24-247.ap-northeast-1.compute.internal>
2022-12-02 02:26:19 +0000

    [Fix]admin/posts,users_controllers admin/posts,users_views

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,10 +1,18 @@
 class Admin::PostsController < ApplicationController
   def index
+    @posts = Post.all.page(params[:page]).per(10)
+  end
+
+  def show
   end
 
   def edit
   end
 
-  def show
+  private
+
+  def post_params
+    params.require(:post).permit(:user_id, :shop_name, :shop_explanation, :address, :latitude, :longitude, :is_deleted, :post_image)
   end
+
 end
