27e131c2b1c3398094c9e6538a744f4061f54eb6
EC2 Default User <ec2-user@ip-172-31-24-247.ap-northeast-1.compute.internal>
2022-12-03 00:48:46 +0000

    [Add]view of user_image attached

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,16 @@
 class Admin::UsersController < ApplicationController
   def index
+    @users = User.all.page(params[:page]).per(10)
+    # ページネーションのための記述.per(10)で１ページのうちで表示できるデータ数を10件にする
   end
 
   def show
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :last_name, :first_name, :last_name_kana, :first_name_kana, :user_name, :self_introduction, :is_deleted, :user_image)
+  end
+
 end
