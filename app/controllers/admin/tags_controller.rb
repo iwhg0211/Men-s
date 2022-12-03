b01d1dddc0fb235d83aec486ffab57cc0952c7fc
EC2 Default User <ec2-user@ip-172-31-24-247.ap-northeast-1.compute.internal>
2022-12-03 02:02:52 +0000

    [Add]view of tag's view,new_tag_form

--- app/controllers/admin/tags_controller.rb
+++ app/controllers/admin/tags_controller.rb
@@ -1,7 +1,17 @@
 class Admin::TagsController < ApplicationController
+  
   def index
+    @tags = Tag.all
   end
 
   def edit
+    @tag = Tag.find(params[:id])
   end
+  
+  private
+  
+  def tag_params
+    params.require(:tag).permit(:tag_name)
+  end
+  
 end
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -15,7 +15,7 @@ class Admin::UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
-    redirect_to 
+    redirect_to admin_user_path(@user.id)
   end
 
   private
